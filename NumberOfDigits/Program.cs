74eaa7de7eedfa5de0bd6a448cc984f25303812a
Kristror <daniil.bezy@ya.ru>
2020-11-28 13:39:19 +0500

    Added NumberOfDigits

--- NumberOfDigits/Program.cs
+++ NumberOfDigits/Program.cs
@@ -10,6 +10,40 @@ namespace NumberOfDigits
     {
         static void Main(string[] args)
         {
+            ///
+            ///Домашняя работа Безукладникова Даниила
+            ///
+            /// Написать метод подсчета количества цифр числа.
+            ///
+            int number;
+            int copynum;
+            int digits = 0;
+            Console.WriteLine("Программа по подсчету количества цифр числа");
+
+            Console.WriteLine("Введите  число:");
+            Int32.TryParse(Console.ReadLine(), out number);
+            
+            copynum = number;
+            if (number > 0)
+            {
+                while (number > 0)
+                {
+                    number /= 10;
+                    digits++;
+                }
+            } else if( number < 0)
+                {
+                while (number < 0)
+                {
+                    number /= 10;
+                    digits++;
+                }
+            }
+                else if (number == 0)  digits++; 
+
+            Console.WriteLine($"Число {copynum} имеет {digits} цифр");
+
+            Console.ReadKey();
         }
     }
 }
