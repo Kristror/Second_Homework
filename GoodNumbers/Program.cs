076724def69dd1e265b151a5ccc5210f1e7b8a37
Kristror <daniil.bezy@ya.ru>
2020-11-28 15:04:06 +0500

    Added GoodNumbers

--- GoodNumbers/Program.cs
+++ GoodNumbers/Program.cs
@@ -10,6 +10,31 @@ namespace GoodNumbers
     {
         static void Main(string[] args)
         {
+            ///
+            ///Домашняя работа Безукладникова Даниила
+            ///
+            ///  *Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. Хорошим называется число, которое делится на сумму своих цифр. 
+            ///  Реализовать подсчет времени выполнения программы, используя структуру DateTime.
+            ///
+            Console.WriteLine("Программа для подсчита хороших чисел в диапазоне от 1 до 1 000 000 000");
+            DateTime begin = DateTime.Now;
+            int amount = 0;
+            for (int num = 1; num <= 1000000000; num++)
+            {
+                int sum = 0;
+                int copy = num;
+                while (copy > 0)
+                {
+                    sum += copy % 10;
+                    copy /= 10;                    
+                }
+                if (num%sum == 0) amount ++;
+            }
+            Console.WriteLine($"Кол-во хороших чисел в диапазоне от 1 до 1 000 000 000 : {amount}");
+            Console.WriteLine($"Выполнение программы заняло: {begin - DateTime.Now}");
+
+            Console.ReadKey();
         }
+
     }
 }
