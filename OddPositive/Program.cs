bc374c47d4c340c633f60dd180fc2693b30998ec
Kristror <daniil.bezy@ya.ru>
2020-11-28 13:50:53 +0500

    Added OddPositive

---
+++ OddPositive/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OddPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            ///Домашняя работа Безукладникова Даниила
+            ///
+            ///  С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел
+            ///
+            int num;
+            int sum = 0;
+            Console.WriteLine("Программа для подсчита суммы всех введенных нечетных положительных чисел до введения нуля");
+
+            do
+            {
+                int.TryParse(Console.ReadLine(), out num);
+                sum += (num > 0)&&(num % 2 !=0)? num: 0;
+            } while (num != 0);
+
+            Console.WriteLine($"Cумма всех введенных нечетных положительных чисел: {sum}");
+            Console.ReadKey();            
+        }
+    }
+}
